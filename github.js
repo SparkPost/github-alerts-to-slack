ab5eb02690536c88214ce614684d13b8751624e4
Jasmine Pate <jpate@JasminePatesMBP.attlocal.net>
2021-03-11 12:47:54 -0500

    SA-2217 typo

--- github.js
+++ github.js
@@ -16,7 +16,7 @@ class GitHubClient {
     return `query { 
         search(
           query: "${searchQuery}",
-          type: REPOSITORY, last: 2
+          type: REPOSITORY, last: 50
         ) {
           repositoryCount
           edges {
