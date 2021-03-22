bbd3cb0d35aee88cc16b2e93918a112311d76d46
Jasmine Pate <jpate@JasminePatesMBP.attlocal.net>
2021-03-22 17:42:33 -0400

    disable node_modules cache

--- slack.js
+++ slack.js
@@ -1,13 +1,14 @@
-const got = require('got');
+const got = require("got");
 
 class SlackClient {
-    constructor(webhook) {
-        this.webhook = webhook;
-    }
+  constructor(webhook) {
+    this.webhook =
+      "https://hooks.slack.com/services/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";
+  }
 
-    async postMessage(json) {
-        await got.post(this.webhook, { json });
-    }
+  async postMessage(json) {
+    await got.post(this.webhook, { json });
+  }
 }
- 
+
 module.exports = SlackClient;
