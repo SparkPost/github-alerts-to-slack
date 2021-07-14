a9d0ee244fd87beed57b3d5d66243ec3c36bf22e
Jasmine Pate <jpate@JasminePatesMBP.attlocal.net>
2021-07-13 21:50:46 -0400

    remove tokens

--- slack.js
+++ slack.js
@@ -5,17 +5,12 @@ const { WebClient } = require("@slack/web-api");
 
 class SlackClient {
   constructor() {
-    this.botToken = "xoxb-2166135452-1788032209014-aaqbogxqC9XpyLvxXyetfBj3";
-    this.signingSecret = "7f69dea7c755fea56fafc1490802b0b4";
-    this.apiToken =
-      "xapp-1-A01PJSYLRE0-2262870196802-bb342288f4ab470044aef778ab091afd9dedef75f22a5d0279f3810a6a64694e";
-    this.webhook =
-      "https://hooks.slack.com/services/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";
+    this.slackToken = process.env.SLACK_APP_TOKEN;
     const app = new App({
-      token: this.botToken,
-      signingSecret: this.signingSecret,
+      token: process.env.botToken,
+      signingSecret: process.env.signingSecret,
       socketMode: true,
-      appToken: this.apiToken,
+      appToken: process.env.apiToken,
     });
 
     app.action("acknowledge_button", async ({ ack, body, respond }) => {
@@ -64,7 +59,7 @@ class SlackClient {
   }
 
   async postMessage(json) {
-    await got.post(this.webhook, { json });
+    await got.post(this.slackToken, { json });
   }
 }
 
